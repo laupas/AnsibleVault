93fdb1a50c20d573824e0a93e31078e3184baa10
Pascal Lauener <pascal@lauener.biz>
2020-03-01 16:28:59 +0100

    use LauPas.BuildExtensions

--- build/build.cs
+++ build/build.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
 using static Bullseye.Targets;
-using static SimpleExec.Command;
-
+using static LauPas.BuildExtensions;
+    
 namespace Build
 {
+
     static class Program
     {
         static void Main(string[] args)
@@ -18,47 +19,49 @@ namespace Build
             }));
 
             string buildNumber = string.Empty;
-            string nugetOrgToken = string.Empty;
 
             Target("init", () =>
             {
-                var versionPrefix = Environment.GetEnvironmentVariable("VERSION_PREFIX");
-                var runNumber = Environment.GetEnvironmentVariable("GITHUB_RUN_NUMBER");
-                nugetOrgToken = Environment.GetEnvironmentVariable("NUGET_ORG_API_KEY");
-                if (string.IsNullOrEmpty(runNumber) || string.IsNullOrEmpty(versionPrefix))
+                if (IsEnvVariableExisting("GITHUB_RUN_NUMBER"))
                 {
-                    buildNumber = $"0.0.0-local";
+                    Logger.LogInformation($"BuildAgent run");
+                    SetConfigFile("BuildConfig.yml");
                 }
                 else
                 {
-                    buildNumber = $"{versionPrefix}.{runNumber}";
+                    Logger.LogInformation($"Local run");
+                    SetConfigFile(".BuildConfig.yml");
                 }
-
-                Console.WriteLine($"BuildNumber: {buildNumber}");
+                
+                var versionPrefix = GetConfigValue("VERSION_PREFIX", "0.0");
+                var runNumber = GetConfigValue("GITHUB_RUN_NUMBER", "0-local");
+                buildNumber = $"{versionPrefix}.{runNumber}";
+                Logger.LogInformation($"BuildNumber: {buildNumber}");
             });
 
             Target("restore", DependsOn("init"), () =>
             {
-                Run("dotnet", $"restore", workingDirectory: "./..");
+                RunProcess("dotnet", $"restore", workingDirectory: "./..");
             });
 
             Target("build", DependsOn("init"), () =>
             {
-                Run("dotnet", $"build -c Release -p:Version={buildNumber}", "./..");
+                RunProcess("dotnet", $"build -c Release -p:Version={buildNumber}", "./..");
             });
 
             Target("test", DependsOn("init"), () =>
             {
-                Run("dotnet", $"test -c Release --no-build", "./..");
+                RunProcess("dotnet", $"test -c Release --no-build", "./..");
             });
 
             Target("pack", DependsOn("init"), () =>
             {
-                Run("dotnet", $"pack ./AnsibleVault/AnsibleVault.csproj --no-build -c Release -p:Version={buildNumber}", "./..");
-                Run("dotnet", $"nuget push ./AnsibleVault/bin/Release/*.nupkg --no-symbols true -s https://api.nuget.org/v3/index.json -k {nugetOrgToken}", "./..");
+                var nugetOrgToken = GetConfigValue<string>("NUGET_ORG_API_KEY");
+                RunProcess("dotnet", $"pack ./AnsibleVault/AnsibleVault.csproj --no-build -c Release -p:Version={buildNumber}", "./..");
+                RunProcess("dotnet", $"nuget push ./AnsibleVault/bin/Release/*.nupkg --no-symbols true -s https://api.nuget.org/v3/index.json -k {nugetOrgToken}", "./..");
             });
             
-            RunTargetsAndExit(args);
+            RunTargetAndExit(args);
         }
     }
 }
